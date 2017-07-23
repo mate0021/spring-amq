1f72b3972855b0ede2fbcd0700ddee80b32da178
mate00 <mami.ext@gmail.com>
2017-07-23 10:49:39 +0200

    initial commit. Added Spring Boot support

---
+++ src/main/java/rnd/mate00/springamq/Application.java
@@ -0,0 +1,15 @@
+package rnd.mate00.springamq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by mate00 on 23.07.17.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
